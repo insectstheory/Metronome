cc71d1eebb1eabee65e99e489c786c0aa79367a7
Insects Theory <giovanni_fiorani94@outlook.it>
2026-04-20 21:43:53 +0200

    Update Metronome link.lua

--- Metronome link.lua
+++ Metronome link.lua
@@ -1,3 +1,11 @@
+-- Metronome
+-- Metronome light with
+-- Ableton Link support
+-- E1: BPM (When Link is off)
+-- E2: Bar measure (2-4-8)
+-- K2: Enable/D Ableton Link
+-- K3: Tap tempo
+
 engine.name = "None"
 
 local bpm          = 120.0
